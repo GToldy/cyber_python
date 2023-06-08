504b2a29b9a7e3577b3568ba441132956e662169
zsofi82 <zsofigaal@gmail.com>
2023-06-08 11:34:26 +0200

    Delete unused method

--- app.py
+++ app.py
@@ -32,23 +32,5 @@ def face_recognition():
     return render_template('face-rec.html')
 
 
-
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit a empty part without filename
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-
 if __name__ == '__main__':
     app.run()
